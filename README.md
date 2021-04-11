8ae283743016b705dfc24ac6d53bb935aa103f6c
Neil Palima <neiljan.palima@gmail.com>
2021-04-11 12:00:46 +0800

    heroku setup

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ The backend is only used to serve static content.
 
 ### How to Run the Application
 1. Go to `/frontend` then build the client.
-2. Go to `/backend` then run the server.
+2. Go back to the root directory, run `npm i && npm run build && npm start`.
 3. Access the url `localhost:8080` on your browser.
--- src/entities/server.ts
+++ src/entities/server.ts
@@ -19,10 +19,10 @@ const server = (port: number): Express => {
     res.redirect('/login');
   });
 
-  app.use(express.static(path.join(__dirname, '../../../frontend', 'build')));
+  app.use(express.static(path.join(__dirname, '../../frontend', 'build')));
 
   app.get('/login', (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, '../../../frontend', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../frontend', 'build', 'index.html'));
   });
 
   app.use(errorHandleMiddleware.notFound);
